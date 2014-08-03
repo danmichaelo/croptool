danmichaelo/croptool

commit 0cf9cc03dd877a5685ca0514028ae2f2a4399141

Code cleanup

public_html/backend.php
@@ -1,38 +1,6 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-ini_set('memory_limit', '512M');
-
-require('../vendor/autoload.php');
-#require('../oauth.php');
-#require('../BorderLocator.php');
-
-
-/*************************************************
- * Routing
- *************************************************/
-
-$hostname = isset($_SERVER['HTTP_X_FORWARDED_SERVER'])
-                ? $_SERVER['HTTP_X_FORWARDED_SERVER']
-                : $_SERVER['SERVER_NAME'];
-
-if ($hostname == 'tools.wmflabs.org, tools-eqiad.wmflabs.org' || $hostname == 'tools-eqiad.wmflabs.org') {
-    $hostname = 'tools.wmflabs.org';
-}
-
-$basepath = dirname( $_SERVER['SCRIPT_NAME'] );
-$testingEnv = ($hostname !== 'tools.wmflabs.org');
-
-session_name('croptool');
-session_set_cookie_params(0, $basepath, $hostname);
-session_start();
-
-if (isset($_GET['title'])) {
-    // Store the title, so we can retrieve if after
-    // having having authenticated at the OAuth endpoint
-    $_SESSION['title'] = $_GET['title'];
-}
+require 'common.php';
 
 /**
  * A file containing the following keys:

public_html/common.php
@@ -1,0 +1,35 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('memory_limit', '512M');
+
+require('../vendor/autoload.php');
+#require('../oauth.php');
+#require('../BorderLocator.php');
+
+
+/*************************************************
+ * Routing
+ *************************************************/
+
+$hostname = isset($_SERVER['HTTP_X_FORWARDED_SERVER'])
+                ? $_SERVER['HTTP_X_FORWARDED_SERVER']
+                : $_SERVER['SERVER_NAME'];
+
+if ($hostname == 'tools.wmflabs.org, tools-eqiad.wmflabs.org' || $hostname == 'tools-eqiad.wmflabs.org') {
+    $hostname = 'tools.wmflabs.org';
+}
+
+$basepath = dirname( $_SERVER['SCRIPT_NAME'] );
+$testingEnv = ($hostname !== 'tools.wmflabs.org');
+
+session_name('croptool');
+session_set_cookie_params(0, $basepath, $hostname);
+session_start();
+
+if (isset($_GET['title'])) {
+    // Store the title, so we can retrieve if after
+    // having having authenticated at the OAuth endpoint
+    $_SESSION['title'] = $_GET['title'];
+}

public_html/index.php
@@ -1,17 +1,8 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-
-//if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'http') {
-//    $redirect = "https://" . $_SERVER['HTTP_X_FORWARDED_SERVER'] . $_SERVER['REQUEST_URI'];
-//    header("Location: $redirect");
-//    exit;
-//}
-
 require('../TsIntuition/ToolStart.php'); // for testing
 //require('/home/project/intuition/src/Intuition/ToolStart.php');
-require('backend.php');
+require('common.php');
 
 // Localization using TsIntuition
 // https://github.com/Krinkle/TsIntuition/wiki/Documentation

src/CropTool.php
@@ -8,6 +8,8 @@ class CropTool {
 
     protected $publicPath;
 
+    public $api;
+
     public function __construct(MwApiClient $apiClient = null, Curl $curl = null)
     {
         $this->publicPath = dirname(dirname(__FILE__)) . '/public_html/';
@@ -28,9 +30,9 @@ class CropTool {
         }
     }
 
-    public function pageExists($title)
+    public function pageExists( $title )
     {
-        return $this->api->pageExists($title);
+        return $this->api->pageExists( $title );
     }
 
     public function logout()

src/MwApiClient.php
@@ -113,12 +113,14 @@ class MwApiClient
     public function pageExists($title)
     {
 
-        $response = $this->request(array(
+        $query = array(
             'action' => 'query',
             'prop' => 'pageprops',
             'format' => 'json',
             'titles' => 'File:' . $title
-        ));
+        );
+
+        $response = $this->request($query);
 
         foreach ($response->query->pages as $pageid => $page) {
             if ($pageid == '-1') {

commit 4da2f4280d9f8873cb6b09a4b4fee72e26d13207

Fix bug introduced in 0cf9cc03
OAuthConsumer needs to be initialized in index.php as well, so it can fetch OAuth callback requests

public_html/backend.php
@@ -2,31 +2,6 @@
 
 require 'common.php';
 
-/**
- * A file containing the following keys:
- * - consumerKey: The "consumer token" given to you when registering your app
- * - consumerSecret: The "secret token" given to you when registering your app
- * - localPassphrase: The (base64 encoded) key used for encrypting cookie content
- * - jpegtranPath: Path to jpegtran
- */
-$configFile = '../config.ini';
-$config = parse_ini_file($configFile);
-
-if ( $config === false ) {
-    header( "HTTP/1.1 500 Internal Server Error" );
-    echo 'The ini file could not be read';
-    exit(0);
-}
-
-if (!isset( $config['consumerKey'] ) || !isset( $config['consumerSecret'] )) {
-    header( "HTTP/1.1 500 Internal Server Error" );
-    echo 'Required configuration directives not found in ini file';
-    exit(0);
-}
-
-Image::$pathToJpegTran = $config['jpegtranPath'];
-
-$oauth = new OAuthConsumer($hostname, $basepath, $testingEnv, $config['consumerKey'], $config['consumerSecret'], $config['localPassphrase']);
 $apiClient = new MwApiClient($oauth);
 $ct = new CropTool($apiClient);
 

public_html/common.php
@@ -33,3 +33,31 @@ if (isset($_GET['title'])) {
     // having having authenticated at the OAuth endpoint
     $_SESSION['title'] = $_GET['title'];
 }
+
+/**
+ * A file containing the following keys:
+ * - consumerKey: The "consumer token" given to you when registering your app
+ * - consumerSecret: The "secret token" given to you when registering your app
+ * - localPassphrase: The (base64 encoded) key used for encrypting cookie content
+ * - jpegtranPath: Path to jpegtran
+ */
+$configFile = '../config.ini';
+$config = parse_ini_file($configFile);
+
+if ( $config === false ) {
+    header( "HTTP/1.1 500 Internal Server Error" );
+    echo 'The ini file could not be read';
+    exit(0);
+}
+
+if (!isset( $config['consumerKey'] ) || !isset( $config['consumerSecret'] )) {
+    header( "HTTP/1.1 500 Internal Server Error" );
+    echo 'Required configuration directives not found in ini file';
+    exit(0);
+}
+
+Image::$pathToJpegTran = $config['jpegtranPath'];
+
+$site = isset($_GET['site']) ? $_GET['site'] : 'no.wikipedia.org';
+
+$oauth = new OAuthConsumer($site, $hostname, $basepath, $testingEnv, $config['consumerKey'], $config['consumerSecret'], $config['localPassphrase']);