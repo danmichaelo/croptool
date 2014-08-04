83e287c2a21e90d9fb7f10fcf1a06e096069a63c
Dan Michael O. Heggø <danmichaelo@gmail.com>
2014-08-04 21:30:59 +0000

    Add server-side http->https redirect

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,11 @@
 <?php
 
+if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'http') {
+    $redirect = "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+    header("Location: $redirect");
+    exit;
+}
+
 require('../TsIntuition/ToolStart.php'); // for testing
 //require('/home/project/intuition/src/Intuition/ToolStart.php');
 require('common.php');
