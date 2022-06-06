9a8e9a2849d3df0513b84137c8b863199fad9dc1
Dan Michael O. Heggø <danmichaelo@gmail.com>
2022-06-06 18:10:05 +0000

    Move jobs to kubernetes

--- jobs/cleanup.sh
+++ jobs/cleanup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 DIR=/data/project/croptool/public_html/files/
 NFILES=`find $DIR -type f -mmin +60 -name '[!.]*' -print -delete | wc -l`
 echo "$(date +'%Y-%m-%d %H:%M') Deleted $NFILES files"
