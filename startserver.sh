6a847b97a32b68fc365b7bba6e328dd26faf2883
Sohom <sohomdatta1+git@gmail.com>
2024-01-23 23:40:48 +0530

    move a bunch of stuff to scripts

--- startserver.sh
+++ startserver.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-pecl install imagemagick
-pecl install exif
-pecl install zip
-
-# curl -A "CropTool/0.1 (https://croptool.toolforge.org)" "http://www.ijg.org/files/jpegsrc.v9a.tar.gz" | tar -xz
-# cd jpeg-*
-# ./configure
-# make
-# make test
-# composer install --optimize-autoloader
 cp config.dist.ini config.ini
-vendor/bin/phpunit
-php generate-key.php
-npx gulp build
 heroku-php-apache2 .
