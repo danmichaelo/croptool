70f7a3a4c682919ffb3fb688bf5a3413a942b796
Sohom <sohomdatta1+git@gmail.com>
2024-01-23 22:22:37 +0530

    fx aptfile

--- startserver.sh
+++ startserver.sh
@@ -3,12 +3,12 @@ pecl install imagemagick
 pecl install exif
 pecl install zip
 
-curl -A "CropTool/0.1 (https://croptool.toolforge.org)" "http://www.ijg.org/files/jpegsrc.v9a.tar.gz" | tar -xz
-cd jpeg-*
-./configure
-make
-make test
-composer install --optimize-autoloader
+# curl -A "CropTool/0.1 (https://croptool.toolforge.org)" "http://www.ijg.org/files/jpegsrc.v9a.tar.gz" | tar -xz
+# cd jpeg-*
+# ./configure
+# make
+# make test
+# composer install --optimize-autoloader
 cp config.dist.ini config.ini
 vendor/bin/phpunit
 php generate-key.php
