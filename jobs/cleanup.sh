ae599e0ba895af87e3af652cab39452149386f0d
Sohom <sohomdatta1+git@gmail.com>
2024-01-31 18:14:26 +0530

    move to apache?

--- jobs/cleanup.sh
+++ jobs/cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-DIR=/data/project//public_files/files/
+DIR=$HOME/public_files/files/
 NFILES=`find $DIR -type f -mmin +60 -name '[!.]*' -print -delete | wc -l`
 echo "$(date +'%Y-%m-%d %H:%M') Deleted $NFILES files"
--- scripts/startserver.sh
+++ scripts/startserver.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-exec /layers/fagiani_apt/apt/usr/sbin/lighttpd -D -f .lighttpd.conf -m /layers/fagiani_apt/apt/usr/lib/lighttpd/
+php generate-key.php
+heroku-php-apache2 -C ./apache.conf public_html/
